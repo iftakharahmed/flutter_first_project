f34a56c297497abf2a6afa5b061d72efd199d50d
iftakharahmed <iftakharahmedtasrif@gmail.com>
2023-03-08 03:02:43 +0600

    App name changed and i tried t chnage the icon but package error

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Iftakhar Ahmed'),
     );
   }
 }
